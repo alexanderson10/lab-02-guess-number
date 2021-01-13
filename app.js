41c1d0cd2c655c0e3ca1d9af7b2bcd6aa4339948
Alexanderson10 <alexanderson1010@me.com>
2021-01-12 16:19:14 -0800

    test

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 // import functions and grab DOM elements
-const submitButton = document.getElementById('guess-button')
-// initialize state
+import { compareNumbers } from './utils.js'
+const guessButton = document.getElementById('guess-button')
+const userInput = document.getElementById('number-guessed')
+const result = document.getElementById('Result')
+const guessesLeft = document.getElementById('guesses-left')
 
+
+// initialize state
+let randomNumber = Math.ceil(Math.random() * 20)
+let userGuesses = 4
+let wins = 0
+let losses = 0
 // set event listeners to update state and DOM
+guessButton.addEventListener('click', () => {
+    console.log(userInput.value, randomNumber)
+    const userNumber = Number(userInput.value)
+    const answer = compareNumbers(userNumber, randomNumber)
+    if (answer === 0) {
+        result.textContent = 'you won'
+        guessesLeft.textContent = `You have ${userGuesses} guesses left`
+    }
+    if (answer === -1) {
+        result.textContent = 'too low'
+        userGuesses--
+        guessesLeft.textContent = `You have ${userGuesses} guesses left`
+    }
+
+    if (answer === 1) {
+        result.textContent = 'too high'
+        userGuesses--
+        guessesLeft.textContent = `You have ${userGuesses} guesses left`
+    }
+
+    console.log(userGuesses)
+})
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,36 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+// import { example } from '../example.js';import
+import { compareNumbers } from '../utils.js'
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('if numbers match it should return 0', (expect) => {
+    const guess = 4;
+    const correctNumber = 4;
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 0;
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = compareNumbers(guess, correctNumber);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+test('if numbers dont match return -1', (expect) => {
+    const guess = 7;
+    const correctNumber = 8;
+    const expected = -1;
+    const actual = compareNumbers(guess, correctNumber);
+    expect.equal(actual, expected);
+
+});
+test('if numbers dont match because guess is too low return 1', (expect) => {
+    const guess = 6
+    const correctNumber = 7;
+    const expected = -1;
+    const actual = compareNumbers(guess, correctNumber);
+    expect.equal(actual, expected);
+})
